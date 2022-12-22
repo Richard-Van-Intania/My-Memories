1b51099bebb852d14710942179065eceaa56eff6
Richard <85865743+Richard-Van-Intania@users.noreply.github.com>
2022-12-22 17:29:16 +0700

    commit jaa

--- app/src/main/java/com/example/mymemories/MainActivity.kt
+++ app/src/main/java/com/example/mymemories/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.mymemories
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymemories.models.BoardSize
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+  private lateinit var recyclerView: RecyclerView
+  private lateinit var textViewMove: TextView
+  private lateinit var textViewPair: TextView
+
+  private var boardSize: BoardSize = BoardSize.EASY
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    recyclerView = findViewById(R.id.recyclerView)
+    textViewMove = findViewById(R.id.textViewMove)
+    textViewPair = findViewById(R.id.textViewPairs)
+
+    recyclerView.adapter = MemoryBoardAdaptor(this, boardSize)
+    recyclerView.setHasFixedSize(true)
+    recyclerView.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+  }
 }
---
+++ app/src/main/java/com/example/mymemories/MemoryBoardAdaptor.kt
@@ -0,0 +1,46 @@
+package com.example.mymemories
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymemories.models.BoardSize
+import kotlin.math.min
+
+class MemoryBoardAdaptor(private val context: Context, private val boardSize: BoardSize) :
+    RecyclerView.Adapter<MemoryBoardAdaptor.ViewHolder>() {
+
+  companion object {
+    private const val MARGIN_SIZE = 10
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    val cardWidth: Int = parent.width / boardSize.getWidth() - (2 * MARGIN_SIZE)
+    val cardHeight: Int = parent.height / boardSize.getHeight() - (2 * MARGIN_SIZE)
+    val cardSideLength: Int = min(cardWidth, cardHeight)
+    val view: View = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+    val layoutParams: ViewGroup.MarginLayoutParams =
+        view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+    layoutParams.width = cardSideLength
+    layoutParams.height = cardSideLength
+    layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+
+    return ViewHolder(view)
+  }
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    holder.bind(position)
+  }
+
+  override fun getItemCount() = boardSize.numCard
+
+  inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
+    fun bind(position: Int) {
+      imageButton.setOnClickListener { println("$position") }
+    }
+  }
+}
